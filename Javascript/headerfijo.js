85e4262ba374d53c035bbc576a783b939dc442f2
jaresma <loowcrik@gmail.com>
2020-01-10 22:26:09 -0700

    firts commit

---
+++ Javascript/headerfijo.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 0){
+            $('header').addClass('headerfijo');
+        } else{
+            $('header').removeClass('headerfijo');
+
+        }
+        ('header')
+      
+    })
+})
